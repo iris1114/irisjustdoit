ef50f6a4d80881fb082001abe3261e1bc93e6234
irischew1114 <qweichew@gmail.com>
2024-07-24 18:00:55 +0800

    update: home page

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,66 +3,80 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import HeroImg from '@/data/hero-1.svg'
+import Image from 'next/image'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 12
 
 export default function Home({ posts }) {
+  console.log({ posts })
   return (
     <>
+      <div className="mb-12 flex flex-col items-center justify-evenly md:flex-row">
+        <div className="order-1 md:w-1/2">
+          <h2 className="py-4 text-2xl md:text-3xl"> Iris Just Do It.</h2>
+          <p>喜歡旅行、美食、挑戰的前端工程師，立志過「想要就 just do it」的生活。</p>
+        </div>
+        <div className=" w-full md:order-1 md:w-1/2">
+          <HeroImg className=" fill-gray-800 dark:fill-gray-500" />
+        </div>
+      </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-4xl md:leading-14">
+            Latest Post
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        <ul className="grid grid-cols-1 gap-4 divide-y divide-gray-200 dark:divide-gray-700 md:grid-cols-3 ">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
+            const { slug, date, title, summary, tags, images } = post
             return (
-              <li key={slug} className="py-12">
+              <li key={slug} className="py-10">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <div className="space-y-2">
+                    {images &&
+                      images.map((image, index) => (
+                        <div key={index}>
+                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                            <Image
+                              alt={title}
+                              src={image}
+                              width={300}
+                              height={400}
+                              className="h-full w-full rounded-lg object-cover"
+                            />
+                          </Link>
+                        </div>
+                      ))}
+                    <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                      <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                        {title}
+                      </Link>
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
+                    </div>
                     <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                       </dd>
                     </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
+                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                      {summary}
                     </div>
+                  </div>
+                  <div className="mt-4 text-base font-medium leading-6">
+                    <Link
+                      href={`/blog/${slug}`}
+                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                      aria-label={`Read more: "${title}"`}
+                    >
+                      Read more &rarr;
+                    </Link>
                   </div>
                 </article>
               </li>
@@ -81,11 +95,11 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
