b176fbabb48e7f999ef2d5c1005325a23102c796
irischew1114 <qweichew@gmail.com>
2024-12-21 23:33:37 +0800

    update: tech blog

--- app/Main.tsx
+++ app/Main.tsx
@@ -6,7 +6,8 @@ import NewsletterForm from 'pliny/ui/NewsletterForm'
 import HeroImg from '@/data/hero-1.svg'
 import Image from 'next/image'
 
-const MAX_DISPLAY = 12
+const MAX_DISPLAY_LIFE = 9
+const MAX_DISPLAY_TECH = 8
 
 export default function Home({ posts }) {
   return (
@@ -16,7 +17,7 @@ export default function Home({ posts }) {
           <h2 className="py-4 text-2xl md:text-3xl"> Iris Just Do It.</h2>
           <p>
             熱愛旅行、挑戰自我、分享經驗，並沉浸於學習與發掘新事物的樂趣，致力於過上「Just Do
-            It」想做就去做的生活。
+            It」想做就去做的生活。。
           </p>
         </div>
         <div className=" w-full md:order-1 md:w-1/2">
@@ -26,71 +27,135 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-4xl md:leading-14">
-            Latest Post
+            Latest Post - Life
           </h1>
         </div>
         <ul className="grid grid-cols-1 gap-4 divide-y divide-gray-200 dark:divide-gray-700 md:grid-cols-3 ">
           {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags, images } = post
-            return (
-              <li key={slug} className="py-10">
-                <article>
-                  <div className="space-y-2">
-                    {images &&
-                      images.map((image, index) => (
-                        <div key={index}>
-                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                            <Image
-                              alt={title}
-                              src={image}
-                              width={300}
-                              height={400}
-                              className="h-full w-full rounded-lg object-cover"
-                            />
-                          </Link>
-                        </div>
-                      ))}
-                    <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                      <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                        {title}
+          {posts
+            .filter((post) => post.tags.includes('life'))
+            .slice(0, MAX_DISPLAY_LIFE)
+            .map((post) => {
+              const { slug, date, title, summary, tags, images } = post
+              return (
+                <li key={slug} className="py-10">
+                  <article>
+                    <div className="space-y-2">
+                      {images &&
+                        images.map((image, index) => (
+                          <div key={index}>
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              <Image
+                                alt={title}
+                                src={image}
+                                width={300}
+                                height={400}
+                                className="h-full w-full rounded-lg object-cover"
+                              />
+                            </Link>
+                          </div>
+                        ))}
+                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                          {title}
+                        </Link>
+                      </h2>
+                      <div className="flex flex-wrap">
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
+                      </div>
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                        </dd>
+                      </dl>
+                      <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        {summary}
+                      </div>
+                    </div>
+                    <div className="mt-4 text-base font-medium leading-6">
+                      <Link
+                        href={`/blog/${slug}`}
+                        className="text-primary-600 hover:text-primary-600 dark:hover:text-primary-500"
+                        aria-label={`Read more: "${title}"`}
+                      >
+                        Read more &rarr;
                       </Link>
-                    </h2>
-                    <div className="flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
                     </div>
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
-                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                      {summary}
-                    </div>
-                  </div>
-                  <div className="mt-4 text-base font-medium leading-6">
-                    <Link
-                      href={`/blog/${slug}`}
-                      className="text-primary-600 hover:text-primary-600 dark:hover:text-primary-500"
-                      aria-label={`Read more: "${title}"`}
-                    >
-                      Read more &rarr;
-                    </Link>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
+                  </article>
+                </li>
+              )
+            })}
         </ul>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-4xl md:leading-14">
+            Latest Post - Tech
+          </h1>
+        </div>
+        <ul className="grid grid-cols-1 gap-4 divide-y divide-gray-200 dark:divide-gray-700 md:grid-cols-2 ">
+          {!posts.length && 'No posts found.'}
+          {posts
+            .filter((post) => post.tags.includes('tech'))
+            .slice(0, MAX_DISPLAY_TECH)
+            .map((post) => {
+              const { slug, date, title, summary, tags, images } = post
+              return (
+                <li key={slug} className="py-1">
+                  <article>
+                    <div className="space-y-2">
+                      {images &&
+                        images.map((image, index) => (
+                          <div key={index}>
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            ></Link>
+                          </div>
+                        ))}
+                      <h2 className="text-xl font-bold leading-8 tracking-tight">
+                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                          {title}
+                        </Link>
+                      </h2>
+                      <div className="flex flex-wrap">
+                        <dl className="mr-4">
+                          <dt className="sr-only">Published on</dt>
+                          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                            <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                          </dd>
+                        </dl>
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
+                      </div>
+                    </div>
+                    {/* <div className="mt-4 text-base font-medium leading-6">
+                      <Link
+                        href={`/blog/${slug}`}
+                        className="text-primary-600 hover:text-primary-600 dark:hover:text-primary-500"
+                        aria-label={`Read more: "${title}"`}
+                      >
+                        Read more &rarr;
+                      </Link>
+                    </div> */}
+                  </article>
+                </li>
+              )
+            })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY_TECH && (
+        <div className="flex justify-end py-8 text-base font-medium leading-6">
           <Link
             href="/blog"
-            className="text-primary-600 hover:text-primary-600 dark:hover:text-primary-500"
+            className="text-2xl text-primary-600 hover:text-primary-600 dark:hover:text-primary-500"
             aria-label="All posts"
           >
             All Posts &rarr;
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,76 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Salonia - Campaign Lading Page',
+    description: `- Custom animation and design for IG campaign page  \n - RWD / HTML / CSS / JS`,
+    imgSrc: '/static/images/projects/salonia.png',
+    href: 'https://landing-page-salonia.netlify.app/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Cardude - Drive Festival Campaign',
+    description: `- Event page where you can vote, participate in test drives.  \n - RWD / React.js / Styled Components`,
+    imgSrc: '/static/images/projects/cardude.png',
+    href: 'https://driving-festival.netlify.app/',
+  },
+  {
+    title: 'Hua Nan Commercial Bank - Financial Calculator Trial Website',
+    description: `- 6 types of financial calculator trails  \n - Form verification, blogs, carousels, and promote page. \n - RWD / Vue.js`,
+    imgSrc: '/static/images/projects/huanan.png',
+    href: 'https://hncb.netlify.app',
+  },
+  {
+    title: "McDonald's - Campaign Lading Page",
+    description: `- The event page in the McDonald’s app  \n - RWD / Vue.js`,
+    imgSrc: '/static/images/projects/mcdonalds.png',
+    href: 'https://mcdonald-campaign.netlify.app/',
+  },
+  {
+    title: 'Redox - Health Product Page',
+    description: `- Custom animation for health product page  \n - RWD / HTML / CSS / JS`,
+    imgSrc: '/static/images/projects/redox.png',
+    href: 'https://redox-landing.netlify.app/',
+  },
+  {
+    title: 'Nike - 360 Degree Effect Product Demo',
+    description: `- StockX 360 degree effect in vanilla JavaScript  \n - RWD / HTML / CSS / JS`,
+    imgSrc: '/static/images/projects/nike.png',
+    href: 'https://360-degree-effect.netlify.app/',
+  },
+  {
+    title: 'Landing Page Coffee 3D',
+    description: `- 3D effect with mouse movement  \n - RWD / HTML / CSS / JS / GSAP`,
+    imgSrc: '/static/images/projects/3dcoffee.png',
+    href: 'https://3d-coffee.netlify.app/',
+  },
+  {
+    title: 'Air Conditioner Page',
+    description: `- React.js / RWD  \n - Styled Components`,
+    imgSrc: '/static/images/projects/aircon.png',
+    href: 'https://react-air-contioner-lading-page.netlify.app/',
+  },
+  {
+    title: 'Easybank Landing Page',
+    description: `- Practice from Frontend Mentor \n - RWD / HTML / CSS / JS`,
+    imgSrc: '/static/images/projects/easybank.png',
+    href: 'https://easy-bank-lading.netlify.app/',
+  },
+  {
+    title: 'OurSky Homepage',
+    description: `- Mobile First methodology \n - RWD / HTML / CSS / JS.`,
+    imgSrc: '/static/images/projects/oursky.png',
+    href: 'https://ousky-homepage.netlify.app/',
+  },
+  {
+    title: 'React Chart',
+    description: `- React charting practice \n - RWD / React / Chart.js`,
+    imgSrc: '/static/images/projects/chart.png',
+    href: 'https://react-chartjs.netlify.app/',
   },
 ]
 
